694ba758df86e98c4c5dd12a78acf8f8036b9738
Lain Iwakura <lain@Lains-MacBook-Air.local>
2022-07-11 00:05:51 +0300

    feat: basic functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,149 @@
 use std::collections::HashMap;
 use std::error::Error;
+use std::fs;
+use std::path::PathBuf;
+use std::process::exit;
 
 use clap::parser::ValuesRef;
 use triple_accel::levenshtein_exp;
+use walkdir::WalkDir;
+
+const TEMPLATES_REPO: &str = "https://github.com/github/gitignore.git";
 
 pub struct IgnoreTemplate {
     pub name: String,
     pub template: String,
 }
 
-/// Update the default templates database
-/// This will download the latest patterns database from the GitHub repository.
-pub fn update() -> Result<(), Box<dyn Error>> {
-    Ok(())
+pub struct TemplateEntry {
+    prefix: String,
+    name: String,
+    template: String,
 }
 
+impl TemplateEntry {
+    pub fn new(prefix: String, name: String, template: String) -> TemplateEntry {
+        TemplateEntry {
+            prefix,
+            name,
+            template,
+        }
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+
+    pub fn prefix(&self) -> &str {
+        &self.prefix
+    }
+
+    pub fn template(&self) -> &str {
+        &self.template
+    }
+
+    pub fn title(&self) -> String {
+        format!("{}:{}", self.prefix(), self.name())
+    }
+
+    pub fn to_string(&self) -> String {
+        let hashes = "#".repeat(self.name().len() + 4);
+
+        format!("
+{hashes}
+# {} #
+{hashes}
+
+{}
+
+
+
+", self.name(), self.template())
+    }
+}
+
+
 /// Get the default templates database
-pub fn get_templates() -> Result<HashMap<String, String>, Box<dyn Error>> {
-    let hash_map = HashMap::from([
-        ("rust".to_string(), "*.rs".to_string()),
-        ("java".to_string(), "*.java".to_string()),
-        ("js".to_string(), "*.js".to_string()),
-        ("ts".to_string(), "*.ts".to_string()),
-    ]);
-    Ok(hash_map)
+pub fn get_templates() -> Result<HashMap<String, TemplateEntry>, Box<dyn Error>> {
+    match get_app_dir() {
+        Some(path) => {
+            // check if path exists
+            if !path.exists() {
+                return Err(Box::new(std::io::Error::new(
+                    std::io::ErrorKind::NotFound,
+                    "Templates path does not exist",
+                )));
+            }
+
+            // filter only files that has .gitignore extension
+            let entries: Vec<TemplateEntry> = WalkDir::new(path)
+                .into_iter()
+                .filter_entry(|e| {
+                    if e.file_type().is_file() || e.file_type().is_symlink() {
+                        return e.path().extension() == Some("gitignore".as_ref());
+                    }
+
+                    !e.path().ends_with(".git") &&
+                        !e.path().ends_with(".github")
+                })
+                .filter_map(|e| match e {
+                    Ok(e) => if e.file_type().is_file() { Some(e) } else { None },
+                    Err(_) => None
+                }
+                )
+                .map(|e| {
+                    let name = e.file_name().to_string_lossy().trim_end_matches(".gitignore").to_string();
+                    let prefix = e.path().parent().unwrap().file_name().unwrap().to_string_lossy().to_string();
+                    let template = fs::read_to_string(e.path()).unwrap();
+
+                    if prefix == "ignore" {
+                        TemplateEntry::new("".to_string(), name, template)
+                    } else {
+                        TemplateEntry::new(prefix.to_lowercase(), name, template)
+                    }
+                })
+                .collect();
+
+            let mut hash_map = HashMap::new();
+            for entry in entries {
+                if hash_map.contains_key(entry.title().as_str()) {
+                    return Err(Box::new(std::io::Error::new(
+                        std::io::ErrorKind::AlreadyExists,
+                        format!("Duplicate template name: {}", entry.name()),
+                    )));
+                }
+
+                hash_map.insert(entry.title(), entry);
+            }
+
+            Ok(hash_map)
+        }
+        None => {
+            Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "Could not find the application directory",
+            )))
+        }
+    }
 }
 
 /// Find the closest string to the given string in the given vector of strings.
 /// Returns None if no suggestion is found.
-pub fn find_closest(target: &str, patterns: Vec<&String>) -> Option<String> {
-    let mut closest_distance = 100;
-    let mut closest_template_name = None;
+pub fn find_closest<'a>(target: &str, patterns: Vec<&'a TemplateEntry>) -> Option<&'a TemplateEntry> {
+    let mut closest_distance = u32::MAX;
+    let mut closest_template = None;
 
 
     for pattern in patterns {
-        let distance = levenshtein_exp(target.as_ref(), pattern.as_ref());
-        if distance < closest_distance {
+        let distance = levenshtein_exp(target.as_ref(), pattern.name().as_ref());
+        if distance < closest_distance && distance < 10 {
             closest_distance = distance;
-            closest_template_name = Some(pattern);
+            closest_template = Some(pattern);
         }
     }
 
-    if let Some(name) = closest_template_name {
-        Some(name.to_string())
+    if let Some(entry) = closest_template {
+        Some(entry)
     } else {
         None
     }
@@ -50,41 +151,148 @@ pub fn find_closest(target: &str, patterns: Vec<&String>) -> Option<String> {
 
 
 /// Generate a gitignore file from the given template names.
-pub fn generate_gitignore(templates: ValuesRef<String>) -> Result<String, String> {
-    let available_templates = get_templates().unwrap();
-    let mut templates_to_ignore: ValuesRef<String> = templates;
+pub fn generate_gitignore(mut templates: ValuesRef<String>) -> Result<String, Box<dyn Error>> {
+    match get_templates() {
+        Ok(available_templates) => {
+            let res = templates.try_fold(
+                "".to_string(),
+                |acc, name| {
+                    if available_templates.contains_key(name) {
+                        Ok(format!("{}{}", acc, &available_templates[name].to_string()))
+                    } else {
+                        let available_templates: Vec<&TemplateEntry> = available_templates
+                            .values()
+                            .collect();
 
-    let mut gitignore = String::new();
+                        let closest = find_closest(name, available_templates);
+                        if let Some(closest) = closest {
+                            Err(Box::new(std::io::Error::new(
+                                std::io::ErrorKind::InvalidInput,
+                                format!("Template '{}' not found, did you mean '{}'?", name, closest.title()),
+                            )))
+                        } else {
+                            Err(Box::new(std::io::Error::new(
+                                std::io::ErrorKind::InvalidInput,
+                                format!("Template '{}' not found", name),
+                            )))
+                        }
+                    }
+                },
+            )?;
 
-    let res: Result<(), String> = templates_to_ignore.try_for_each(|template| {
-        if available_templates.contains_key(template) {
-            gitignore.push_str(
-                &format!(
-                    "{}\n\n",
-                    available_templates[template]
-                )
-            );
+            // trim trailing newlines
+            Ok(res.trim_matches('\n').to_string())
+        }
+        Err(err) => {
+            return Err(err);
+        }
+    }
+}
 
-            Ok(())
-        } else {
-            let available_templates_names: Vec<&String> = available_templates.keys().collect();
-            let suggestion = find_closest(
-                template,
-                available_templates_names,
-            );
+/// Get the application directory
+pub fn get_app_dir() -> Option<PathBuf> {
+    match dirs::config_dir() {
+        Some(path) => Some(path.join("ignore")),
+        None => None
+    }
+}
 
-            let mut err = format!("Unknown template \"{}\"", template);
-            if let Some(suggestion) = suggestion {
-                err.push_str(&format!("\nDid you mean \"{}\"?", suggestion));
+
+/// Clones the templates repository into the default directory
+pub fn clone_templates_repo() -> Result<PathBuf, Box<dyn Error>> {
+    match get_app_dir() {
+        Some(path) => {
+            let target_path = path.join("default");
+
+            // run git clone
+            let output = std::process::Command::new("git")
+                .arg("clone")
+                .arg(TEMPLATES_REPO)
+                .arg(target_path.to_str().unwrap())
+                .output()?;
+
+            // check if clone was successful
+            if !output.status.success() {
+                return Err(Box::new(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Failed to clone templates repository",
+                )));
             }
 
-            Err(err)
+            Ok(target_path)
         }
-    });
-
-    if res.is_err() {
-        Err(res.err().unwrap())
-    } else {
-        Ok(gitignore)
+        None => Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "Could not find app directory",
+        )))
     }
+}
+
+/// Update the default templates database
+/// This will download the latest patterns database from the GitHub repository.
+pub fn pull_templates_repo() -> Result<PathBuf, Box<dyn Error>> {
+    match get_app_dir() {
+        Some(path) => {
+            let target_path = path.join("default");
+
+            // run git pull
+            let output = std::process::Command::new("git")
+                .arg("-C")
+                .arg(target_path.to_str().unwrap())
+                .arg("pull")
+                .arg("origin")
+                .arg("main")
+                .output()?;
+
+            // check if pull was successful
+            if !output.status.success() {
+                return Err(Box::new(std::io::Error::new(
+                    std::io::ErrorKind::Other,
+                    "Failed to pull templates repository",
+                )));
+            }
+
+            Ok(target_path)
+        }
+        None => Err(Box::new(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "Could not find app directory",
+        )))
+    }
+}
+
+/// Initialize the default templates database
+pub fn init_default_templates() -> Result<(), Box<dyn Error>> {
+    match get_app_dir() {
+        Some(path) => {
+            if !path.exists() {
+                fs::create_dir_all(path)?;
+                println!("Cloning templates repository, first time only");
+                clone_templates_repo()?;
+            }
+
+            Ok(())
+        }
+        None => {
+            return Err(Box::new(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "Could not find app directory",
+            )));
+        }
+    }
+}
+
+/// Checks if the given command is available in the system path.
+pub fn command_is_available(name: &str) -> bool {
+    let output = std::process::Command::new("which")
+        .arg(name)
+        .output()
+        .ok();
+
+    output.is_some()
+}
+
+pub fn error(msg: &str) {
+    eprintln!("{}", msg);
+    exit(1);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::process::exit;
-
 use clap::command;
+use clap::parser::ValuesRef;
 
-use ignore::generate_gitignore;
+use ignore::{error, generate_gitignore, get_templates, init_default_templates};
 
 fn main() {
     let cmd = command!("ignore")
+        .about("Generate a gitignore file from the given template names.")
         .arg(
             clap::Arg::with_name("template")
                 .short('t')
@@ -14,6 +14,14 @@ fn main() {
                 .exclusive(true)
                 .takes_value(true)
                 .multiple_values(true),
+        )
+        .arg(
+            clap::Arg::with_name("list")
+                .short('l')
+                .long("list")
+                .help("List all available templates")
+                .takes_value(false)
+                .exclusive(true),
         )
         .arg(
             clap::Arg::with_name("update")
@@ -27,19 +35,48 @@ fn main() {
     let matches = cmd.get_matches();
 
     if let Some(templates) = matches.get_many("template") {
-        match generate_gitignore(templates) {
-            Ok(gitignore) => {
-                println!("{}", gitignore);
-                exit(0);
-            }
-            Err(err) => {
-                eprintln!("{}", err);
-                exit(1);
-            }
-        }
+        handle_template_argument(templates);
+    } else if matches.is_present("list") {
+        handle_list_argument();
     } else if matches.is_present("update") {
-        println!("Updating patterns database");
+        hand_update_argument()
     } else {
         println!("No pattern provided");
     }
+}
+
+fn handle_template_argument(templates: ValuesRef<String>) -> () {
+    if let Err(err) = init_default_templates() {
+        error(err.to_string().as_str())
+    }
+
+    match generate_gitignore(templates) {
+        Ok(gitignore) => {
+            println!("{}", gitignore);
+        }
+        Err(err) => {
+            error(err.to_string().as_str());
+        }
+    }
+}
+
+fn handle_list_argument() {
+    if let Err(err) = init_default_templates() {
+        error(err.to_string().as_str());
+    }
+
+    match get_templates() {
+        Ok(available_templates) => {
+            for (_, entry) in available_templates {
+                println!("{}", entry.title());
+            }
+        }
+        Err(err) => {
+            error(err.to_string().as_str());
+        }
+    }
+}
+
+fn hand_update_argument() {
+    println!("Updating patterns database");
 }
